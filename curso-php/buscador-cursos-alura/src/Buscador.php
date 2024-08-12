a8fd7f918858fce059e13b394a035e54629b93ae
Cleiton dos Santos Cunha <102325156+cleitoncunha04@users.noreply.github.com>
2024-08-12 16:00:51 -0300

    Vi as formas de importar arquivo no PHP com o autoload (PSR-4, classmap e files)

--- curso-php/buscador-cursos-alura/src/Buscador.php
+++ curso-php/buscador-cursos-alura/src/Buscador.php
@@ -3,6 +3,7 @@
 namespace Cleitoncunha04\BuscadorCursosAlura;
 
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Component\DomCrawler\Crawler;
 
 class Buscador
@@ -10,34 +11,43 @@ class Buscador
     /**
      * @var ClientInterface
      */
-    private $httpClient;
+    //private $httpClient;
     /**
      * @var Crawler
      */
-    private $crawler;
+    //private $crawler;
 
-    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    public function __construct(
+        public readonly ClientInterface $httpClient,
+        public readonly Crawler         $crawler
+    )
     {
-        $this->httpClient = $httpClient;
-        $this->crawler = $crawler;
     }
 
     public function buscarConteudo(string $url): array
     {
-        $resposta = $this->httpClient->request('GET', $url);
-
-        $html = $resposta->getBody();
-
-        $this->crawler->addHtmlContent($html, 'UTF-8');
-
-        //gerei uma lista de cursos pesquisando pelo seletor CSS
-        $elementosCursos = $this->crawler->filter('span.card-curso__nome');
-
         $cursos = [];
 
-        //a lista retornou um "DOM", então fiz um foreach para pegar os textContent's (títulos dos cursos)
-        foreach ($elementosCursos as $elemento) {
-            $cursos[] = $elemento->textContent;
+        try {
+            $resposta = $this->httpClient->request('GET', $url);
+
+            $html = $resposta->getBody();
+
+            $this->crawler->addHtmlContent($html, 'UTF-8');
+
+            //gerei uma lista de cursos pesquisando pelo seletor CSS
+            $elementosCursos = $this->crawler->filter('span.card-curso__nome');
+
+            //a lista retornou um "DOM", então fiz um foreach para pegar os textContent's (títulos dos cursos)
+            foreach ($elementosCursos as $elemento) {
+                $cursos[] = $elemento->textContent;
+            }
+        } catch (GuzzleException $e) {
+            $cursos[] = "Ocorreu um erro ao buscar o curso: " . $e->getMessage();
+        }
+
+        if(count($cursos) == 0) {
+            $cursos[] = "Nenhum curso encontrado";
         }
 
         return $cursos;
