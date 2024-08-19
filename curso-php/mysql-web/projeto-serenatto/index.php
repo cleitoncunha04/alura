bdf21d502b2163d5d625a894574395042ca33e90
Cleiton Cunha <cleiton_sc_04@hotmail.com>
2024-08-19 17:35:39 -0300

    Pegando os dados do banco

--- curso-php/mysql-web/projeto-serenatto/index.php
+++ curso-php/mysql-web/projeto-serenatto/index.php
@@ -1,57 +1,19 @@
 <?php
-$produtosCafe = [
-    [
-        'nome' => 'Café Cremoso',
-        'descricao' => 'Café cremoso irresistivelmente suave e que envolve seu paladar',
-        'preco' => "5.00",
-        'imagem' => "img/cafe-cremoso.jpg"
-    ],
-    [
-        'nome' => 'Café com leite',
-        'descricao' => 'A harmonia do café e do leite, uma experiência reconfortante',
-        'preco' => "2.00",
-        'imagem' => "img/cafe-com-leite.jpg"
-    ],
-    [
-        'nome' => "Cappuccino",
-        'descricao' => "Café suave, leite cremoso e uma pitada de sabor adocicado",
-        'preco' => "7.00",
-        'imagem' => "img/cappuccino.jpg"
-    ],
-    [
-        'nome' => "Café Gelado",
-        'descricao' => "Café gelado refrescante, adoçado e com notas sutis de baunilha ou caramelo.",
-        'preco' => "3.00",
-        'imagem' => "img/cafe-gelado.jpg"
-    ]
-];
 
-$produtosAlmoco = [
-    [
-        'nome' => "Bife",
-        'descricao' => "Bife, arroz com feijão e uma deliciosa batata frita.",
-        'preco' => "27.90",
-        'imagem' => "img/bife.jpg"
-    ],
-    [
-        'nome' => "Filé de peixe",
-        'descricao' => "Filé de peixe salmão assado, arroz, feijão verde e tomate.",
-        'preco' => "24.99",
-        'imagem' => "img/prato-peixe.jpg"
-    ],
-    [
-        'nome' => "Frango",
-        'descricao' => "Saboroso frango à milanesa com batatas fritas, salada de repolho e molho picante.",
-        'preco' => "23.00",
-        'imagem' => "img/prato-frango.jpg"
-    ],
-    [
-        'nome' => "Fettuccine",
-        'descricao' => "Prato italiano autêntico da massa do fettuccine com peito de frango grelhado.",
-        'preco' => "22.50",
-        'imagem' => "img/fettuccine.jpg"
-    ]
-];
+use Vendor\Serenatto\Conexao;
+
+require_once 'vendor/autoload.php';
+
+$conexao = Conexao::getConexao();
+
+$statement = $conexao->query('SELECT * FROM serenatto WHERE tipo = "Café"');
+
+$produtosCafe =$statement->fetchAll();
+
+var_dump($produtosCafe);
+
+exit();
+
 ?>
 
 <!doctype html>
